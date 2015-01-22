5a46a7ae71812ae0c027a79019dde77800861a7b
Tim Becker <tim@press-any-key.de>
2015-01-22 23:00:01 +0100

    API-367 added json_schema validator

--- lib/validate/validate.rb
+++ lib/validate/validate.rb
@@ -11,28 +11,57 @@
 #
 require 'pathname'
 require 'json-schema'
+require 'json_schema'
 
 module Fidor
   module SchemaValidation
-    
-    class JsonDashSchemaValidator
-      def initialize files_to_validate
-        @files_to_validate = files_to_validate
-        @meta_schema_fn    = "#{File.dirname(__FILE__)}/schema.json"
-        validate_all
-      end # init
+    class Validator
       def validate_all
-        @files_to_validate.each do |file|
+         @files_to_validate.each do |file|
           schema = File.read file
           puts "validating: #{file}"
           validate schema
         end
       end
+    end
+    class JsonDashSchemaValidator < Validator
+      def initialize files_to_validate
+        @files_to_validate = files_to_validate
+        @meta_schema_fn    = "#{File.dirname(__FILE__)}/schema.json"
+        validate_all
+      end # init
       def validate schema
         begin
           JSON::Validator.validate!(@meta_schema_fn, schema, :strict => true)
         rescue
           puts $!
+          puts "Failed!"
+        else
+          puts "Passed"
+        end
+      end
+    end
+
+    class JsonUnderscoreSchemaValidator < Validator
+      def initialize files_to_validate
+        @files_to_validate = files_to_validate 
+        schema_data = File.read("#{File.dirname(__FILE__)}/schema.json")
+        schema_json = JSON.parse(schema_data)
+        @schema = JsonSchema.parse!(schema_json)
+        validate_all
+      end 
+
+      def validate schema
+        begin
+                schema_to_test = JSON.parse(schema)
+                @schema.validate! schema_to_test
+                puts "Passed validation against meta-schema!"
+                JsonSchema.parse!(schema_to_test)
+        rescue
+          puts $!
+          puts "Failed!"
+        else
+          puts "Passed!"
         end
       end
     end
@@ -48,7 +77,10 @@ module Fidor
     
 
     def self.main
+      puts "JSON-Schema"
       JsonDashSchemaValidator.new find_all_schema
+      puts "JSON_Schema"
+      JsonUnderscoreSchemaValidator.new find_all_schema
     end
   end
 end
